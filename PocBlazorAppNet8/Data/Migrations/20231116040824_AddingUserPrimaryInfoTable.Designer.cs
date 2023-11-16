96f9aacd4db1a2fe250cf175009953b0e531e601
Yohan Garcia <yoanvaldes03@gmail.com>
2023-11-15 23:12:52 -0500

    - added the table UserPrimaryInfo

--- PocBlazorAppNet8/Data/Migrations/20231116040824_AddingUserPrimaryInfoTable.Designer.cs
+++ PocBlazorAppNet8/Data/Migrations/20231116040824_AddingUserPrimaryInfoTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PocBlazorAppNet8.Data;
 
@@ -11,9 +12,11 @@ using PocBlazorAppNet8.Data;
 namespace PocBlazorAppNet8.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231116040824_AddingUserPrimaryInfoTable")]
+    partial class AddingUserPrimaryInfoTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
